c6d9c9601b186718de9b15a73e6425ea6de7abbe
Myth <caipengbo@outlook.com>
2019-06-03 09:48:15 +0800

    Weekly Contest 139 Only Problem 1071

--- LeetCodeWeeklyContest/src/WeeklyContest139.java
+++ LeetCodeWeeklyContest/src/WeeklyContest139.java
@@ -4,6 +4,7 @@
  * Created by Myth on 6/2/2019
  */
 public class WeeklyContest139 {
+    // 1071 题目
     public static int getRepet(String ss) {
         int len=ss.length();
         int next[]=new int [len+1];
@@ -37,21 +38,57 @@ public class WeeklyContest139 {
             return "";
         }
     }
-    public static String gcdOfStrings(String str1, String str2) {
+    // 我的思路：去掉 两个串的前缀，剩下的部分，再次对比，直到一个串为空（规定），另一个就是GCD
+    // 错误代码：
+    public static String gcdOfStrings3(String str1, String str2) {
         int len1 = str1.length(), len2 = str2.length();
         if (len1 == 0 || len2 == 0) return "";
 
         if (len1 >= len2 ) {
-            int mod = len1 % len2;
-            if (str1.substring(0, len1-mod).equals())
+            return isRep(str1, str2);
         } else {
-
+            return isRep(str2, str1);
         }
 
-
+    }
+    public static String isRep(String str1, String str2) {
+        int len1 = str1.length(), len2 = str2.length();
+        int mod = len1 % len2;
+        if (mod == 0) {
+            for (int i = 0; i < len1; i+=len2) {
+                if (!str1.substring(i, i+len2).equals(str2)) {
+                    return "";
+                }
+            }
+            return str2;
+        }
+        if (str1.substring(0, len1-mod).equals(str2)) {
+            String sub = str1.substring(len1-mod, len1);
+            int lenSub = sub.length();
+            if (len2 % lenSub != 0) return "";
+            else {
+                for (int i = 0; i < len2; i+=lenSub) {
+                    if (!str2.substring(i, i+lenSub).equals(sub)) {
+                        return "";
+                    }
+                }
+                return sub;
+            }
+        } else {
+            return "";
+        }
+    }
+    // 简洁正确的代码
+    public static String gcdOfStrings1(String str1, String str2) {
+        if (str1.length() < str2.length()) return gcdOfStrings1(str2, str1);
+        else if (!str1.startsWith(str2)) return "";
+        else if (str2.length() == 0) return str1;
+        else return gcdOfStrings1(str1.substring(str2.length()), str2);
     }
 
     public static void main(String[] args) {
-        System.out.println(WeeklyContest139.gcdOfStrings("ABABAB", "ABAB"));
+        String str1 = "TAUXXTAUXXTAUXXTAUXXTAUXX";
+        String str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX";
+        System.out.println(WeeklyContest139.gcdOfStrings1(str1, str2));
     }
 }
