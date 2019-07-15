ef77e7e54d851c59520809743500c1875ad5d5a3
Myth <caipengbo@outlook.com>
2019-07-15 17:35:59 +0800

    Add util snippets(for debug)

---
+++ LeetCode/util/Input.java
@@ -0,0 +1,23 @@
+package util;
+
+import java.io.*;
+import java.util.List;
+
+/**
+ * Title: 从文件中读取数据, 构建链表或者树
+ * Desc: [1, 2, 3]
+ * Created by Myth on 5/15/2019
+ */
+public class Input {
+    public static BufferedReader getBufferReader() {
+        File file = new File("src/input.txt");
+        InputStreamReader inputStreamReader = null;
+        try {
+            inputStreamReader = new InputStreamReader(new FileInputStream(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        return bufferedReader;
+    }
+}
