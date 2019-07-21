dca97558b15f76e3e96510bb9ef9f5a5628121fc
Myth <caipengbo@outlook.com>
2019-07-21 16:51:29 +0800

    Partition

---
+++ LeetCode/search/P131PalindromePartitioning.java
@@ -0,0 +1,15 @@
+package search;
+
+import java.util.List;
+
+/**
+ * Title: 131. 分割回文串
+ * Desc: 给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。返回 s 所有可能的分割方案。
+ * Created by Myth on 7/21/2019
+ */
+public class P131PalindromePartitioning {
+    // 典型的回溯分割
+    public List<List<String>> partition(String s) {
+
+    }
+}
