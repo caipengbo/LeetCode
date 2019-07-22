843bdcfc67b806bc918ef3e93bc0069139442fe0
Myth <caipengbo@outlook.com>
2019-07-22 10:17:38 +0800

    Partition

--- LeetCode/search/P131PalindromePartitioning.java
+++ LeetCode/search/P131PalindromePartitioning.java
@@ -1,5 +1,6 @@
 package search;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,7 +10,69 @@ import java.util.List;
  */
 public class P131PalindromePartitioning {
     // 典型的回溯分割
-    public List<List<String>> partition(String s) {
+    // 循环：i 从1 到 len(最大分割)，代表分割0个-len 个
+    // 剪枝：待分割的串长度 小于 len
+    // 结束：start == len
+    // 分割：从start往后分割
+    private void backtracking(String s, int start, List<String> cur, List<List<String>> ans) {
+        if (start == s.length()) {
+            ans.add(new ArrayList<>(cur));
+            return;
+        }
+        for (int i = 0; i <= s.length(); i++) {
+            if (start+i > s.length()) break;
+            String segment = s.substring(start, start+i);
+            if (isPalindrome(segment)) {
+                cur.add(segment);
+                backtracking(s, start+i, cur, ans);
+                cur.remove(cur.size()-1);
+            }
+        }
+    }
+    // 是否为回文串：正读反读都一样
+    private boolean isPalindrome(String segment) {
+        if (segment == null || segment.length() == 0) return false;
+        int i = 0, j = segment.length()-1;
+        while (i <= j) {
+            if (segment.charAt(i) != segment.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
 
+    private void backtracking2(String s, int start, List<String> cur, List<List<String>> ans) {
+        if (start == s.length()) {
+            ans.add(new ArrayList<>(cur));
+            return;
+        }
+        for (int i = start; i < s.length(); i++) {
+            if (isPalindrome(s, start, i)) {
+                cur.add(s.substring(start, i+1));
+                backtracking2(s, i+1, cur, ans);
+                cur.remove(cur.size()-1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end)
+            if(s.charAt(start++) != s.charAt(end--)) return false;
+        return true;
+    }
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        List<String> cur = new ArrayList<>();
+        backtracking2(s, 0, new ArrayList<>(), ans);
+        return ans;
+    }
+    public static void main(String[] args) {
+        P131PalindromePartitioning p131 = new P131PalindromePartitioning();
+        // 测试：
+        //"aab" ====> [["a","a","b"],["aa","b"]]
+        //"a" ====> [["a"]]
+        //"" ====> [[]]
+        //"bb" ====> [["b","b"],["bb"]]
+        System.out.println(p131.partition(""));
     }
 }
