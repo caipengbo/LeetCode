706eff16849ab82c1609949904b2ff1d7bbaf8e6
Myth <caipengbo@outlook.com>
2019-09-01 21:53:54 +0800

    Rotated Sorted Array 3, Binary search

---
+++ LeetCode/bisearch/P162FindPeakElement.java
@@ -0,0 +1,19 @@
+package bisearch;
+
+/**
+ * Title: 162. 寻找峰值
+ * Desc:  峰值元素是指其值大于左右相邻值的元素。
+ *
+ * 给定一个输入数组 nums，其中 nums[i] ≠ nums[i+1]，找到峰值元素并返回其索引。
+ *
+ * 数组可能包含多个峰值，在这种情况下，返回任何一个峰值所在位置即可。
+ *
+ * 你可以假设 nums[-1] = nums[n] = -∞。
+ *
+ * Created by Myth on 9/1/2019
+ */
+public class P162FindPeakElement {
+    public int findPeakElement(int[] nums) {
+
+    }
+}
