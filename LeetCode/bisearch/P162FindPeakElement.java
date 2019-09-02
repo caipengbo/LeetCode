a64101ec6404bfbc441a46844f5a65634510560c
Myth <caipengbo@outlook.com>
2019-09-02 10:36:17 +0800

    Find Peak, Binary search

--- LeetCode/bisearch/P162FindPeakElement.java
+++ LeetCode/bisearch/P162FindPeakElement.java
@@ -1,7 +1,7 @@
 package bisearch;
 
 /**
- * Title: 162. 寻找峰值
+ * Title: 162. 寻找峰值（找局部最大值和852题一模一样）
  * Desc:  峰值元素是指其值大于左右相邻值的元素。
  *
  * 给定一个输入数组 nums，其中 nums[i] ≠ nums[i+1]，找到峰值元素并返回其索引。
@@ -14,6 +14,23 @@ package bisearch;
  */
 public class P162FindPeakElement {
     public int findPeakElement(int[] nums) {
+        if (nums.length == 0) return -1;
+        if (nums.length == 1) return 0;
+        int l = 0, r = nums.length - 1;
+        int m;
+        while (l < r) {
+            m = l + (r - l) / 2;
+            if (nums[m] > nums[m+1]) r = m;
+            else l = m + 1;
+        }
+        return l;
+    }
 
+    public static void main(String[] args) {
+        P162FindPeakElement p162 = new P162FindPeakElement();
+        int[] nums1 = {1, 2, 3, 1};
+        int[] nums2 = {1, 2, 1, 3, 5, 6, 4};
+        System.out.println(p162.findPeakElement(nums1));
+        System.out.println(p162.findPeakElement(nums2));
     }
 }
