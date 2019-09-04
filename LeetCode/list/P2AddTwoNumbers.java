77cca336a46f28c75060308af1281d4a00624c25
Myth <caipengbo@outlook.com>
2019-09-04 22:18:37 +0800

    List

--- LeetCode/list/P2AddTwoNumbers.java
+++ LeetCode/list/P2AddTwoNumbers.java
@@ -12,27 +12,36 @@ public class P2AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode p = l1, q = l2, r = null;
         ListNode head = null;
-        int carry = 0;
+        int sum, carry = 0;
         while (p != null && q != null) {
+            sum = (p.val + q.val + carry) > 9 ? (p.val + q.val + carry - 10) : (p.val + q.val + carry);
+            carry = (p.val + q.val + carry) > 9 ? 1 : 0;
             if (r == null) {
-                r = new ListNode((p.val + q.val + carry) / 10);
+                r = new ListNode(sum);
                 head = r;
             } else {
-                r.next = new ListNode((p.val + q.val + carry) / 10);
+                r.next = new ListNode(sum);
                 r = r.next;
             }
-            carry = (p.val + q.val + carry) % 10;
             p = p.next;
             q = q.next;
         }
         while (p != null) {
-            r.next = new ListNode((p.val + carry) / 10);
-            carry = (p.val + carry) % 10;
+            sum = (p.val + carry) > 9 ? (p.val + carry - 10) : (p.val + carry);
+            carry = (p.val + carry) > 9 ? 1 : 0;
+            r.next = new ListNode(sum);
             r = r.next;
+            p = p.next;
         }
         while (q != null) {
-            r.next = new ListNode((q.val + carry) / 10);
-            carry = (q.val + carry) % 10;
+            sum = (q.val + carry) > 9 ? (q.val + carry - 10) : (q.val + carry);
+            carry = (q.val + carry) > 9 ? 1 : 0;
+            r.next = new ListNode(sum);
+            r = r.next;
+            q = q.next;
+        }
+        if (carry != 0) {
+            r.next = new ListNode(carry);
             r = r.next;
         }
         return head;
@@ -41,7 +50,11 @@ public class P2AddTwoNumbers {
     public static void main(String[] args) {
         ListNode l1 = ListUtil.stringToListNode("[2,4,3]");
         ListNode l2 = ListUtil.stringToListNode("[5,6,4]");
+
+        ListNode l3 = ListUtil.stringToListNode("[1,8]");
+        ListNode l4 = ListUtil.stringToListNode("[0]");
         P2AddTwoNumbers p2 = new P2AddTwoNumbers();
-        ListUtil.prettyPrintLinkedList(p2.addTwoNumbers(l1, l2));
+        // ListUtil.prettyPrintLinkedList(p2.addTwoNumbers(l1, l2));
+        ListUtil.prettyPrintLinkedList(p2.addTwoNumbers(l3, l4));
     }
 }
