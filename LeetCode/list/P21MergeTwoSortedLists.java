59209fd0f0dbb2a85778ba0492abb0d07fa46d2e
Myth <caipengbo@outlook.com>
2019-09-09 09:36:22 +0800

    Recursive merge sorted lists

--- LeetCode/list/P21MergeTwoSortedLists.java
+++ LeetCode/list/P21MergeTwoSortedLists.java
@@ -3,7 +3,7 @@ package list;
 import util.ListNode;
 
 /**
- * Title: 21. 合并两个有序链表
+ * Title: 21. 合并两个有序链表(循环写法和递归写法)
  * Desc: 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
  * Created by Myth on 9/8/2019
  */
@@ -32,5 +32,16 @@ public class P21MergeTwoSortedLists {
             p = p.next;
         }
         return head.next;
+    }
+    public ListNode mergeTwoListsRecursive(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoListsRecursive(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoListsRecursive(l1, l2.next);
+            return l2;
+        }
     }
 }
--- LeetCode/list/P23MergeKSortedLists.java
+++ LeetCode/list/P23MergeKSortedLists.java
@@ -7,11 +7,12 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
- * Title: 23. 合并K个排序链表
+ * Title: 23. 合并K个排序链表(使用堆和不使用堆)
  * Desc: 合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
  * Created by Myth on 9/8/2019
  */
 public class P23MergeKSortedLists {
+    // 使用堆
     public ListNode mergeKLists(ListNode[] lists) {
         int k = lists.length;
         if (k == 0) return null;
@@ -42,6 +43,30 @@ public class P23MergeKSortedLists {
         }
         p.next = null;  // 必须加这个封尾操作，有可能会出现环
         return head.next;
+    }
+    // 不使用堆
+    public ListNode mergeKLists2(ListNode[] lists) {
+        return mergeKLists2(lists, 0, lists.length - 1);
+    }
+    public ListNode mergeKLists2(ListNode[] lists, int start, int end) {
+        if (start == end) return lists[start];
+        else if (start < end) {
+            int mid = start + (end - start) / 2;
+            ListNode left = mergeKLists2(lists, start, mid);
+            ListNode right = mergeKLists2(lists, mid+1, end);
+            return mergeTwoListsRecursive(left, right);
+        } else return null;
+    }
+    public ListNode mergeTwoListsRecursive(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoListsRecursive(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoListsRecursive(l1, l2.next);
+            return l2;
+        }
     }
     // [[-2,-1,-1,-1],[]]
     public static void main(String[] args) {
