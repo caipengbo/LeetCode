fd6d6975b1b1f34de92a5cf816dddf08c4176c30
Myth <caipengbo@outlook.com>
2019-09-11 15:21:06 +0800

    Manipulate list

---
+++ LeetCode/list/P19RemoveNthNodeFromEnd.java
@@ -0,0 +1,46 @@
+package list;
+
+import util.ListNode;
+
+/**
+ * Title: 19. 删除链表的倒数第N个节点
+ * Desc: 给定一个链表，删除链表的倒数第 n 个节点（n保证有效, 不会等于0 哦），并且返回链表的头结点。使用一趟扫描
+ * Created by Myth on 9/11/2019
+ */
+public class P19RemoveNthNodeFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) return null;
+        int count = 0;
+        ListNode fast = head, slow = head, pre = head;
+        while (fast != null) {
+            count++;
+            if (count > n) {
+                pre = slow;
+                slow = slow.next;
+            }
+            fast = fast.next;
+        }
+        if (slow == head) {
+            head = head.next;
+        } else {
+            pre.next = slow.next;
+            slow.next = null;
+        }
+        return head;
+    }
+    // 简化：使用dummy，不用pre指针，直接slow指向要删除的节点的前面位置,使用 n 递减计数
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        if (head == null) return null;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode fast = dummy, slow = dummy;
+        while (fast.next != null) {
+            if (n <= 0) slow = slow.next;
+            fast = fast.next;
+            n--;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+
+}
---
+++ LeetCode/list/P328OddEvenNode.java
@@ -0,0 +1,35 @@
+package list;
+
+import util.ListNode;
+import util.ListUtil;
+
+/**
+ * Title: 328. 奇偶链表
+ * Desc: 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+ *
+ *      请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+ *
+ *      应当保持奇数节点和偶数节点的相对顺序。
+ *      链表的第一个节点视为奇数节点，第二个节点视为偶数节点，以此类推。
+ * Created by Myth on 9/11/2019
+ */
+public class P328OddEvenNode {
+    public static ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) return head;
+        ListNode p = head, q;
+        ListNode evenHead = head.next;
+        while (p.next != null && p.next.next != null) {
+            q = p.next;
+            p.next = q.next;
+            p = p.next;
+            q.next = p.next;
+        }
+        p.next = evenHead;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = ListUtil.stringToListNode("[1,2,3,4,5,6,7,8]");
+        ListUtil.prettyPrintLinkedList(P328OddEvenNode.oddEvenList(head));
+    }
+}
