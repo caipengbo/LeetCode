98e01b85b3e3005fb06e450b56188d9ccc48d06d
Myth <caipengbo@outlook.com>
2019-09-12 14:39:10 +0800

    Split list

--- LeetCode/list/P328OddEvenNode.java
+++ LeetCode/list/P328OddEvenNode.java
@@ -4,7 +4,7 @@ import util.ListNode;
 import util.ListUtil;
 
 /**
- * Title: 328. 奇偶链表
+ * Title: 328. 奇偶链表（链表的拆分）
  * Desc: 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
  *
  *      请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
@@ -16,15 +16,16 @@ import util.ListUtil;
 public class P328OddEvenNode {
     public static ListNode oddEvenList(ListNode head) {
         if (head == null || head.next == null || head.next.next == null) return head;
-        ListNode p = head, q;
+        ListNode p = head, q = head.next;
         ListNode evenHead = head.next;
-        while (p.next != null && p.next.next != null) {
-            q = p.next;
-            p.next = q.next;
+        while (p.next != null && p.next.next != null) {  // 判断条件是 p.next,也就是说，是 到 链表的最后一个元素，这个时候需要进行封尾操作
+            p.next = p.next.next;
+            q.next = q.next.next;
             p = p.next;
-            q.next = p.next;
+            q = p.next;
         }
-        p.next = evenHead;
+        p.next = evenHead;  // 封尾操作(该题比较特殊，如果是单纯的将一个链表拆成一个，需要进行封尾)
+        // 因为 p.next == null 跳出循环， p
         return head;
     }
 
