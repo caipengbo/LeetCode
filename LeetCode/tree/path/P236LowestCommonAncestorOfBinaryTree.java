b66feb057931bea27cb1ae8f65ba1ea28d1dd34f
Myth-PC <caipengbo@outlook.com>
2019-10-01 14:27:32 +0800

    LCA

--- LeetCode/tree/path/P236LowestCommonAncestorOfBinaryTree.java
+++ LeetCode/tree/path/P236LowestCommonAncestorOfBinaryTree.java
@@ -8,11 +8,34 @@ import util.TreeNode;
  * Created by Myth on 9/30/2019
  */
 public class P236LowestCommonAncestorOfBinaryTree {
-    // 递归1：对每个节点对应的子树，若该子树不含有p或q，返回nullptr；
+    // 递归1：
+    private TreeNode lca;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        lca = null;
+        find(root, p, q);
+        return lca;
+    }
+    // 左边，右边，或者当前节点，有两个能找到p或者q，那么此节点就是公共祖先
+    private boolean find(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return false;
+        int left = find(root.left, p, q) ? 1 : 0;
+        int right = find(root.right, p, q) ? 1 : 0;
+        int mid = (p == root || q == root) ? 1: 0;
+        if (left + right + mid >= 2) lca = root;
+        return (left + right + mid) > 0;
+    }
+    // 递归2：对每个节点对应的子树，若该子树不含有p或q，返回nullptr；
     // 否则，如果p和q分别位于当前子树根节点两侧，则返回当前节点，
     // 否则（p和q在同一侧，或者只有某一侧有p或q）返回来自左边或右边的LCA。
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == root || q == root) return root;
+        // 左边存在p或者q
+        TreeNode left = lowestCommonAncestor2(root.left, p, q);
+        // 右边存在p或者q
+        TreeNode right = lowestCommonAncestor2(root.right, p, q);
+        // p,q分别位于两侧
+        if (left != null && right != null) return root;
+        return (left == null) ? right : left;
     }
 
 }
