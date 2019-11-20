c620657063365f0fdc0ccd99a352827b6e94d471
Myth <caipengbo@outlook.com>
2019-11-20 17:09:16 +0800

    Sequence DP

---
+++ LeetCode/dp/P121BestTimeToBuyAndSellStock.java
@@ -0,0 +1,18 @@
+package dp;
+
+/**
+ * Title: 121.买卖股票的最佳时机
+ * Desc: (剑指offer 63题)
+ * Created by Myth-Lab on 11/20/2019
+ */
+public class P121BestTimeToBuyAndSellStock {
+    // 保存前面的profit, 保存minVal
+    public int maxProfit(int[] prices) {
+        int profit = 0, minVal = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] <= minVal) minVal = prices[i];
+            else profit = Math.max(profit, prices[i]-minVal);
+        }
+        return profit;
+    }
+}
