263f7946948aaa399cbc650514cdcae3f08a0040
Myth-AliCloud <caipengbo@outlook.com>
2019-12-06 16:56:17 +0800

    DP

---
+++ src/dp/chessboard/P120Triangle.java
@@ -0,0 +1,53 @@
+package dp.chessboard;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Title: 120. 
+ * Desc: 三角形最小路径和 
+ * Created by Myth on 12/06/2019 in VSCode
+ */
+
+
+public class P120Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size(), m;
+        int[] dp = new int[n];  // 使用滚动数组优化
+        dp[0] = triangle.get(0).get(0);
+        for (int i = 1; i < n; i++) {
+            m = triangle.get(i).size();
+            
+            for (int j = m-1; j >= 0; j--) {
+                if (j == 0) dp[j] = dp[0] + triangle.get(i).get(j);
+                else if (j == m-1) dp[j] = dp[j-1] + triangle.get(i).get(j);
+                else dp[j] = Math.min(dp[j], dp[j-1]) + triangle.get(i).get(j);
+            }
+            // For Debug
+            for (int j = 0; j < n; j++) {
+                System.out.print(dp[j] + ", ");
+            }
+            System.out.println();
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            min = Math.min(min, dp[i]);
+        }
+        return min;
+    }
+    public static void main(String[] args) {
+        P120Triangle p120 = new P120Triangle();
+        List<Integer> list0 = Arrays.asList(2);
+        List<Integer> list1 = Arrays.asList(3, 4);
+        List<Integer> list2 = Arrays.asList(6, 5, 7);
+        List<Integer> list3 = Arrays.asList(4, 1, 8, 3);
+        List<List<Integer>> triangle = new LinkedList<>();
+        triangle.add(list0);
+        triangle.add(list1);
+        triangle.add(list2);
+        triangle.add(list3);
+        System.out.println(p120.minimumTotal(triangle));
+    }
+}
---
+++ src/dp/chessboard/P64MinimumPathSum.java
@@ -0,0 +1,31 @@
+package dp.chessboard;
+
+/**
+* Title: 64. 最小路径和
+* Desc: 给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+        说明：每次只能向下或者向右移动一步。
+* Created by Myth on 12/06/2019 in VSCode
+*/
+
+public class P64MinimumPathSum {
+    public int minPathSum(int[][] grid) {
+        // m > 0, n > 0
+        int m = grid.length, n = grid[0].length;
+        if (m < 1 || n < 1) return 0;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j]; 
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
