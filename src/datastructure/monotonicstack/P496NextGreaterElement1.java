af823711ee882a6d38352879d4cc3ab04ec5f9a9
Myth-AliCloud <caipengbo@outlook.com>
2020-01-09 11:06:45 +0800

    Monotonic Stack

---
+++ src/datastructure/monotonicstack/P496NextGreaterElement1.java
@@ -0,0 +1,66 @@
+package datastructure.monotonicstack;
+
+import java.util.Stack;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;;
+
+/**
+* Title: 496. 下一个更大元素 I
+* Desc: 
+* Created by Myth on 01/09/2020 in VSCode
+*/
+
+public class P496NextGreaterElement1 {
+    // 注意两种方法统计方法存的东西
+    // push的时候统计(从后往前)
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        int i = nums2.length-1;
+        int[] ret = new int[nums1.length];
+        while (i >= 0) {
+            if (stack.empty()) {
+                map.put(nums2[i], -1);
+                stack.push(i--);
+            } else if (nums2[stack.peek()] > nums2[i]) {
+                map.put(nums2[i], nums2[stack.peek()]);
+                stack.push(i--);
+            } else {
+                stack.pop();
+            }
+        }
+        for (int j = 0; j < nums1.length; j++) {
+            ret[j] = map.get(nums1[j]);
+        }
+        return ret;
+    }
+    // 推荐此种写法，比较直观
+    // pop的时候统计（√）
+    public int[] nextGreaterElement2(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        int i = 0, n = nums2.length;
+        int[] ret = new int[nums1.length];
+        while (i < n) {
+            if (stack.empty() || stack.peek() > nums2[i]) {
+                stack.push(nums2[i++]);
+            } else {
+                map.put(stack.pop(), nums2[i]);
+            }
+        }
+        for (int j = 0; j < nums1.length; j++) {
+            ret[j] = map.getOrDefault(nums1[j], -1);
+        }
+        return ret;
+    }
+    public static void main(String[] args) {
+        P496NextGreaterElement1 p496 = new P496NextGreaterElement1();
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+        int[] nums11 = {2, 4};
+        int[] nums22 = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(p496.nextGreaterElement(nums1, nums2)));
+        System.out.println(Arrays.toString(p496.nextGreaterElement(nums11, nums22)));
+    }
+}
---
+++ src/datastructure/monotonicstack/P503NextGreaterElement2.java
@@ -0,0 +1,45 @@
+package datastructure.monotonicstack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * Title: 503. 下一个更大元素 II Desc: 给定一个循环数组（最后一个元素的下一个元素是数组的第一个元素），输出每个元素的下一个更大元素。
+ * 数字 x 的下一个更大的元素是按数组遍历顺序，这个数字之后的第一个比它更大的数，这意味着你应该循环地搜索它的下一个更大的数。如果不存在，则输出 -1。
+ * Created by Myth on 01/09/2020 in VSCode
+ */
+
+public class P503NextGreaterElement2 {
+    // 2n
+    // 循环数组
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ret = new int[n];
+        Arrays.fill(ret, -1);
+        Stack<Integer> stack = new Stack<>();  // 递减栈, 存放下标
+        int i = 0, j;
+        // 遍历两遍数组即可
+        while (i < 2*n) {
+            j = (i >= n ? i-n : i);
+            if (stack.empty() || nums[stack.peek()] >= nums[j]) {
+                stack.push(j);
+                i++;
+            } else {
+                ret[stack.pop()] = nums[j];
+            }
+        }
+        return ret;
+    }
+    // 简洁的写法
+    public int[] nextGreaterElements2(int[] A) {
+        int n = A.length, res[] = new int[n];
+        Arrays.fill(res, -1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n * 2; i++) {
+            while (!stack.isEmpty() && A[stack.peek()] < A[i % n])
+                res[stack.pop()] = A[i % n];
+            stack.push(i % n);
+        }
+        return res;
+    }
+}
