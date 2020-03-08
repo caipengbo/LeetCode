7c057bf679d33f035ef4c3ed117c5eb0fa08c9af
Myth-AliCloud <caipengbo@outlook.com>
2020-03-08 15:20:10 +0800

    knapsack

--- src/dp/knapsack/P322CoinChange.java
+++ src/dp/knapsack/P322CoinChange.java
@@ -53,6 +53,34 @@ public class P322CoinChange {
         return dp[amount] > amount ? -1 : dp[amount];
     }
 
+    // 第二遍，
+    // 数组内存放的是最小硬币数
+    public int coinChange4(int[] coins, int amount) {
+        if (coins == null || coins.length == 0 || amount < 0) return -1;
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        int n = coins.length;
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (dp[j] != Integer.MAX_VALUE && coins[i] <= amount-j) dp[j+coins[i]] = Math.min(dp[j+coins[i]], dp[j] + 1);
+            } 
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+    public int coinChange5(int[] coins, int amount) {
+        if (coins == null || coins.length == 0 || amount < 0) return -1;
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int j = 0; j <= amount; j++) {
+                if (j >= coin) dp[j] = Math.min(dp[j], dp[j-coin] + 1);
+            } 
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
     public static void main(String[] args) {
         P322CoinChange p322 = new P322CoinChange();
         int[] coins = {1, 2, 5};
--- src/search/bfs/P994RottingOrange.java
+++ src/search/bfs/P994RottingOrange.java
@@ -1,5 +1,6 @@
 package search.bfs;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 /**
