17270c8da1f7e9fe0d3a576b3ac4f3a6a3630eb0
caipengbo-mac <caipengbo@outlook.com>
2020-04-18 22:43:08 +0800

    P289

---
+++ src/datastructure/array/P289LifeGame.java
@@ -0,0 +1,13 @@
+package datastructure.array;
+
+/**
+* Title: 289. 生命游戏
+* Desc: 纪念因为2020年新冠状病毒去世的英国数学家 约翰·何顿·康威
+* Created by Myth-MBP on 18/04/2020 in VSCode
+*/
+
+public class P289LifeGame {
+    public void gameOfLife(int[][] board) {
+
+    }
+}
