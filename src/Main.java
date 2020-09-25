73aa2d5ca37516ecc7489a86b22c1c956b83f0e1
caipengbo-mac <caipengbo@outlook.com>
2020-09-25 13:54:00 +0800

    change main

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,31 @@
 
 import java.util.*;
 
-
 public class Main
 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-            max = Math.max(max, arr[i]);
+        // int n = sc.nextInt();
+        // System.out.println(ugly(n));
+        String line = sc.nextLine();
+        String[] numStrs = line.split(",");
+        int[] nums = new int[numStrs.length];
+        int i = 0;
+        for (String numStr : numStrs) {
+            nums[i++] = Integer.parseInt(numStr);
         }
-        System.out.println(level(arr, n, max));
+        System.out.println(p3(nums));
     }
+    private static int p3(int[] nums) {
+        if (nums.length == 0) return 0;
+        int[] dp = new int[nums.length+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 2; i <= nums.length; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i-1]);
+        }
+        return dp[nums.length];
+    }
+
     
-    private static int level(int[] arr, int n, int max) {
-        if (n == 1) {
-            return 1;
-        }
-        int sum = 0;
-        for (int i = 1; i <= max; i++) {
-            int l = 0;
-            for (int j = 0; j < n; j++) {
-                if (arr[j] < i) {
-                    l++;
-                }
-            }
-            if (arr[n-1] >= i) {
-                l++;
-            }
-            // System.out.println("==" + l);
-            sum += l;
-        }
-        return sum;
-    }   
 }
